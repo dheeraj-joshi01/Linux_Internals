51909c0bafe152fcb3c6930ddd006bbcfd35f74e
dheeraj-joshi01 <98100333+dheeraj-joshi01@users.noreply.github.com>
2022-03-30 11:24:41 +0530

    dynamic library

---
+++ MMU_and_Lib/dynamic_Lib/calculate.h
@@ -0,0 +1,4 @@
+double mean(double, double);
+double sub(double, double);
+double add(double, double);
+double multiply(double, double);
---
+++ MMU_and_Lib/dynamic_Lib/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+#include "calculate.h"
+
+int glb = 1022;
+
+int main(int argc, char *argv[])
+{
+	double v1, v2, m1, m2, m3, m4;
+	
+	v1 = 45.3;
+	v2 = 7.9;
+	
+	m1=mean(v1,v2);
+	m2 = sub(v1,v2);
+	//m3 = add(v1,v2);
+	//m4 = multiply(v1,v2);
+	
+	printf("The mean of %3.3f and %3.2f is %3.2f\n", v1, v2, m1);
+	printf("The difference of %3.3f and %3.2f is %3.2f\n", v1, v2, m2);
+	//printf("The addn of %3.3f and %3.2f is %3.2f\n", v1, v2, m3);
+	//printf("The multiplication of %3.3f and %3.2f is %3.2f\n", v1, v2, m4);
+	
+	printf("global = %d\n", glb);
+			
+	return 0;
+	
+}
