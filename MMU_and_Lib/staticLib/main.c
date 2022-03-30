020d4f9ebedde1f1b8fe1f9228cb278b4677a1ce
dheeraj-joshi01 <98100333+dheeraj-joshi01@users.noreply.github.com>
2022-03-30 11:16:10 +0530

    cpp programs using static library

---
+++ MMU_and_Lib/staticLib/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+#include "calculate.h"
+
+
+int main(int argc, char *argv[])
+{
+	double v1, v2, m1, m2, m3, m4;
+	
+	v1 = 5.2;
+	v2 = 7.9;
+	
+	m1=mean(v1,v2);
+	m2 = sub(v1,v2);
+	m3 = add(v1,v2);
+	m4 = multiply(v1,v2);
+		
+	printf("The mean of %3.3f and %3.2f is %3.2f\n", v1, v2, m1);
+	printf("The difference of %3.3f and %3.2f is %3.2f\n", v1, v2, m2);
+	printf("The addn of %3.3f and %3.2f is %3.2f\n", v1, v2, m3);
+	printf("The multiplication of %3.3f and %3.2f is %3.2f\n", v1, v2, m4);
+			
+	return 0;
+	
+}
